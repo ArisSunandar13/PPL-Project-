8a2f99cb90249790d9ab3c5fe5178da29e977eaa
Aris Sunandar <aris.s.sy13@gmail.com>
2022-11-22 23:04:03 +0700

    Initial project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCt0vJB3WsAiC42ksdTsAJDShkw_iRhW9c",
+    authDomain: "ppl-project-e30ef.firebaseapp.com",
+    projectId: "ppl-project-e30ef",
+    storageBucket: "ppl-project-e30ef.appspot.com",
+    messagingSenderId: "741595008191",
+    appId: "1:741595008191:web:d61919d721c4c122088a0a",
+    measurementId: "G-0NV898S8QG"
+  }
 };
 
 /*
